25d8eb20bec001c6e097733c13dfb0dad369e037
Aliaksei Kuzmin2 <aliaksei_kuzmin2@epam.com>
2017-01-05 15:08:13 +0300

    Add chain of responsibility pattern implementation

---
+++ patterns/behavioral/chain_of_responsibility/chain_of_response.scala
@@ -0,0 +1,45 @@
+// Avoid coupling the sender of the request to the receiver by giving more than one object
+// the chance to handle the request
+
+abstract class Request
+case class StringRequest(s: String) extends Request
+
+
+abstract class Handler {
+  val handler: Option[Handler]
+  def handle(req: Request) {
+    if (handler.isDefined) handler.get.handle(req)
+  }
+}
+
+
+class ConcreteHandler1(override val handler: Option[Handler]) extends Handler {
+  override def handle(req: Request) {
+    req match {
+      case StringRequest("1") => println("First Concrete")
+      case _ => handler map (_.handle(req))
+    }
+  }
+}
+
+
+class ConcreteHandler2(override val handler: Option[Handler]) extends Handler {
+  override def handle(req: Request) {
+    req match {
+      case StringRequest("2") => println("Second Concrete")
+      case _ => handler map (_.handle(req))
+    }
+  }
+}
+
+object Client {
+  def main(args: Array[String]) {
+    println("Hello")
+
+    val h2 = new ConcreteHandler2(Option(null))
+    val h1 = new ConcreteHandler1(Option(h2))
+  
+    h1.handle(StringRequest("1"))
+    h1.handle(StringRequest("2"))
+  }
+}
